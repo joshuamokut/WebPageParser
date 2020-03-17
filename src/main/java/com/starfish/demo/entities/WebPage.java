f29d7378b8679c275484531bbc32eb557680711e
Jmokut <joshuamokut@gmail.com>
2020-03-17 11:16:02 +0300

    refactoring

--- src/main/java/com/starfish/demo/entities/WebPage.java
+++ src/main/java/com/starfish/demo/entities/WebPage.java
@@ -35,7 +35,8 @@ public class WebPage {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (o == null || getClass() != o.getClass())
+            return false;
         WebPage webPage = (WebPage) o;
         return id.equals(webPage.id);
     }
