62a56f5a0cd8590cc1b32ae6480550ec8f045675
Jmokut <joshuamokut@gmail.com>
2020-03-18 08:10:37 +0300

    Change return type to custom DTO

--- src/main/java/com/starfish/demo/controllers/WebPageController.java
+++ src/main/java/com/starfish/demo/controllers/WebPageController.java
@@ -3,8 +3,6 @@ package com.starfish.demo.controllers;
 
 import com.starfish.demo.DTO.WebPageSetDTO;
 import com.starfish.demo.services.WebPageService;
-import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,6 +27,6 @@ public class WebPageController {
     @GetMapping("add")
     ResponseEntity<String> addParseResult(@RequestParam String weblink,
                                           @RequestParam int size) {
-        return webPageService.requestParse(weblink, size);
+        return webPageService.requestParse(weblink, size).getResponseEntity();
     }
 }
