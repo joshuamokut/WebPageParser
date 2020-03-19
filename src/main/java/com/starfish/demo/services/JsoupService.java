f3a09a08abf1bf3bf0941d1786e5b31d72431f4d
Jmokut <joshuamokut@gmail.com>
2020-03-19 09:57:50 +0300

    Ok Jsoup fails to fetch some url because some sites have a sniffing agent         //and they can figure out that it's java trying to access their site. So         // they restrict access to their site. That's why we have to use UserAgent

--- src/main/java/com/starfish/demo/services/JsoupService.java
+++ src/main/java/com/starfish/demo/services/JsoupService.java
@@ -11,7 +11,11 @@ import java.io.IOException;
 @Component
 public class JsoupService {
     public Document getDoc(String weblink) throws IOException {
-        return Jsoup.connect(weblink).timeout(30000).userAgent("Mozilla/17.0").get();
+        return Jsoup.connect(weblink).userAgent("Mozilla").get();
+        //Ok Jsoup fails to fetch some url because some sites have a sniffing agent
+        //and they can figure out that it's java trying to access their site. So
+        // they restrict access to their site. That's why we have to use UserAgent
+
     }
 
 }
